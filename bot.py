4d004fe6d62a4758acfa94456baee5203837b169
EPFFORCE <38656336+mouncg@users.noreply.github.com>
2020-06-16 10:22:02 +0100

    code cleanup C:

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ with open('config.json') as f:
     data = json.load(f)
 token = str(data["discordBotToken"])
 
+
 async def compileGarage(username):
     profile = await garageScript.compileProfileAsync(username)
 
@@ -19,33 +20,42 @@ async def compileGarage(username):
     height = 30 + (ceil(len(profile["data"]['garage']) / 30) * 291)
     with Image.new('RGBA', (width, height)) as img:
 
-            # Pasting lots
+        # Pasting lots
         with Image.open('OtherImports\parking_spots_all.png') as lots:
             for i in range(ceil(len(profile["data"]['garage']) / 30)):
                 img.paste(lots, (12, 15 + (291 * i)), lots)
 
-        garage = numpy.reshape(profile["data"]['garage'], (ceil(len(profile["data"]['garage']) / 15), 15))
+        garage = numpy.reshape(profile["data"]['garage'],
+                               (ceil(len(profile["data"]['garage']) / 15), 15))
         for y, row in enumerate(garage):
             for x, id in enumerate(row):
 
-                    # Skipping blanks
+                # Skipping blanks
                 if not id:
                     continue
 
-                        # Looking for garage car in cars
+                    # Looking for garage car in cars
                 id = int(id)
-                car_details = [c for c in profile["data"]['cars'] if c[0] == id and c[1] == 'owned']
+                car_details = [
+                    c for c in profile["data"]['cars']
+                    if c[0] == id and c[1] == 'owned'
+                ]
                 if not car_details:
                     continue
 
                 car_details = car_details[0]
-                car = await garageScript.compileCarAsync(id, car_details[2], 'small')
+                car = await garageScript.compileCarAsync(
+                    id, car_details[2], 'small')
                 with Image.open(car) as c:
                     width = c.size[1]
                     length = c.size[0]
-                    with c.rotate(90 if y % 2 == 0 else -90, expand=True, resample=Image.NEAREST) as temp:
+                    with c.rotate(90 if y % 2 == 0 else -90,
+                                  expand=True,
+                                  resample=Image.NEAREST) as temp:
                         _x = 12 + ((x * 61) + (30 - floor(width / 2)))
-                        _y = 20 + (48 - floor(length / 2)) + (y * 181 - (floor(y / 2) * 71))
+                        _y = 20 + (48 -
+                                   floor(length / 2)) + (y * 181 -
+                                                         (floor(y / 2) * 71))
                         img.paste(temp, (_x, _y))
 
                 car.close()
@@ -57,38 +67,38 @@ async def compileGarage(username):
 
 async def carLink(username):
     url = await garageScript.carLink(username)
-    return(str(url))
+    return (str(url))
 
 
-
-
-
-bot = commands.Bot(command_prefix=str(data["botPrefix"]), activity = discord.Game(name=f"{str(data["botPrefix"])}garage [username]"))
-
+bot = commands.Bot(
+    command_prefix=str(data["botPrefix"]),
+    activity=discord.Game(name=f"{str(data['botPrefix'])}garage [username]"))
 
 
 @bot.command()
 async def garage(ctx, username):
     username = str(username)
-    if(".com" in username):
+    if (".com" in username):
         usernameList = username.split("/")
-        username = usernameList[len(usernameList)-1]
-    elif('@' in username):
+        username = usernameList[len(usernameList) - 1]
+    elif ('@' in username):
         username = username.replace("@", "")
     try:
         await ctx.send("Compiling garage of " + username)
         async with ctx.typing():
             await compileGarage(username)
-        await ctx.send(file = discord.File("garage.png"))
+        await ctx.send(file=discord.File("garage.png"))
     except:
         await ctx.send("Error! Could not compile garage.")
+
+
 @bot.command()
 async def car(ctx, username):
     username = str(username)
-    if(".com" in username):
+    if (".com" in username):
         usernameList = username.split("/")
-        username = usernameList[len(usernameList)-1]
-    elif('@' in username):
+        username = usernameList[len(usernameList) - 1]
+    elif ('@' in username):
         username = username.replace("@", "")
     try:
         link = await carLink(username)
@@ -96,17 +106,21 @@ async def car(ctx, username):
     except:
         await ctx.send("Could not send car! Please try again!")
 
+
 @bot.command()
-async def find(ctx,carName):
+async def find(ctx, carName):
     carName = str(carName)
     try:
         carDict = garageScript.findCar(carName)
-        if(len(carDict) <= 0):
+        if (len(carDict) <= 0):
             await ctx.send("I found no cars matching that name!")
             return
         for x in carDict:
-            await ctx.send(f'__**{x}:**__ https://www.nitrotype.com/cars/{carDict[x]}_large_1.png')
+            await ctx.send(
+                f'__**{x}:**__ https://www.nitrotype.com/cars/{carDict[x]}_large_1.png'
+            )
     except:
         await ctx.send("Invalid car! Please try again.")
+
 
 bot.run(token)
--- garageScript.py
+++ garageScript.py
@@ -2,11 +2,217 @@ import json
 from io import BytesIO
 import asyncio
 import aiohttp
-carsdict = {"Lamborgotti Mephisto SS" : 1, "Lamborgotti Mephisto" : 2, "Jeepers Rubricorn" : 3, "Portch Picante" : 4, "Bantly Super Sport" : 5, "The Rolls" : 6, "Winston Citroen" : 7, "Winston Agile" : 8, "Rental Car" : 9, "Mission Accomplished" : 10, "Buggani Vyrus SS" : 11, "Auttie B9" : 13, "Nitsua Lance 722" : 14, "Misoux Lion" : 15, "Misoux Toad" : 16, "Minnie the Cooper" : 17, "Nizza 350x" : 18, "One Ace" : 19, "Cougar Ace" : 20, "Rand Rover R/T" : 21, "B-Team Van" : 22, "Mercedex Bens V-20" : 23, "Mercedex Bens C-64" : 24, "Portch Spyder" : 25, "Auttie Roadster" : 26, "Bimmer M2.0" : 27, "Bimmer 9.0t" : 28, "Thunder Cougarbird" : 29, "Rat Rod Skully" : 30, "Outtie R11" : 31, "The Flamerod" : 33, "Valent Performo" : 34, "Portch GT3 RS" : 35, "Ponce de Leon" : 36, "'67 Shellback GT-500" : 37, "Road Warrior" : 38, "Linux Elise" : 39, "'69 Shellback RT-500" : 40, "The Gator" : 42, "Bastok Suprillia" : 43, "The Judge" : 44, "The Stallion" : 45, "The Macro" : 46, "The Fastback" : 47, "The Covenant" : 48, "The Trifecta" : 49, "8 Bit Racer" : 50, "Mini Sherman" : 51, "Typiano Pizza Car" : 52, "Rocket Man" : 53, "All Terrain Vehicle" : 54, "MP 427" : 55, "The Wambulance" : 56, "The Hotdog Mobile" : 57, "F-35 JSF" : 58, "NASA Shuttle" : 59, "Caterham Racer" : 60, "Mack Daddy" : 61, "Big Hauler" : 62, "Big Blue" : 63, "Fort GT40" : 64, "Dom Vipper GST-R" : 65, "Alpha Romero 8Ω" : 66, "Blazing Buggy" : 67, "F4U Corsair" : 68, "Rocket Sleigh" : 69, "XMaxx Tree Racer" : 70, "Shadow XMaxx Tree" : 71, "Party Sleigh" : 72, "Zonday Tricolore" : 73, "The Monster" : 74, "Flux Capacitor" : 75, "The Gotham" : 76, "The Pirc" : 77, "Suziki GXRS 1200" : 78, "EZ Rider" : 79, "Lamborgotti AdventX" : 80, "Summer Classic" : 81, "Hang Ten" : 82, "'41 Woodie Deluxx" : 83, "Hang Eleven" : 84, "'41 Woodie Sunshine" : 85, "The Xcelsior V12" : 86, "'68 Roadtripper" : 87, "Hang Fifteen" : 88, "Wach 6" : 89, "Fort F-125" : 90, "Wisker Electric" : 91, "'67 Vette" : 92,"MSG 01" : 93, "Fort Stallion" : 94, "Police Bimmer" : 95, "Auttie R-8.1" : 96, "Wampus" : 97, "Pumpkin Hauler" : 98, "Wreath Racer" : 99, "Santa's Buggy" : 100, "Travis' Car" : 101, "Dark Elf" : 102, "The Golden Gift" : 103, "Corndogs Car": 104, "14 Mantaray" : 105, "Ferreti Samsher 458" : 106, "Lacan Hypersport" : 107, "Sun Buggie" : 108, "Hammer Wheels" : 110, "Kringle 4000" : 111, "Buddy's Snowmobile" : 112, "Kringle 4000 XL" : 113, "Buddy's Snowmorocket" : 114, "Six Four" : 115, "Six Four Plus Three" : 116, "The Midnight Hauler" : 117, "The Candy Hauler" : 118, "Kringle 5000" : 119, "Wrapped Wracer" : 120, "Wrapped Wracer GT" : 121, "Holiday Hero" : 122, "Kringle 5000 L.T." : 123, "Mercedex McLaro SLR" : 124, "Floaty Blue" : 125, "B.O.A.T." : 126, "I'm Spicy!" : 127, "Y.A.C.H.T." : 128, "Mercedex McLaro SLR 12.5" : 129, "Nitr-o'-Lantern" : 130, "Nitr-o'-the-Wisp" : 131, "Xmaxx Xxpress" : 132, "XMaxx Xxpress XXL" : 133, "Gilded Xxpress" : 134, "Lamborgotti Xmaxx LT" : 135, "Lamborgotti Xmaxx LT-C" : 136, "Mercedex McLaro SHS 15.0" : 137, "Strykist 1300" : 138, "Range Runner" : 139, "Strykist 1300 XT-LR" : 140, "Track-o-Lantern" : 141, "Gingerbread Racer" : 142, "Gingerbread Racer H&T" : 143, "Missile Toe" : 144, "Missile Toe H&T" : 145, "The Dark Chocolate Knight" : 146, "Tegglsa" : 149, "Egg Beater" : 150, "Eggcedes" : 151, "Egg Hauler" : 152, "Mercedex GT 20.0" : 153, "Rocky Roo" : 154, "NitroPAC" : 155, "Matchbox" : 156, "Lucky Number 7" : 157, "Easy Breezy" : 158, "HoverJet 5000 Mk. 3" : 159, "Golden Breeze" : 160, "B.U.S." : 161, "S'cool B.U.S." : 162, "AU-79" : 163, "The Underachiever" : 164, "The Overachiever" : 165, "The Wildflower" : 166, "Jolly RS" : 167, "Jolly GTX LG" : 168, "The Goldray" : 169, "can hav nt g0ld plx?" : 170, "The Wraptor" : 171, "Travis' Truck" : 172, "The Wraptor GG" : 173, "The Silent Knight" : 174, "NT Gold" : 175,
-"Lamborgotti Tiesto" : 176, "Portch Cobalt" : 177, "Alpha Romero 123Ω" : 178, "Travis' Big Truck" : 179, "Bright Idea" : 180, "The Sandstorm" : 181, "The Jury" : 182, "The Goldfish" : 183, "Shock Value" : 184, "Gold Standard" : 185, "Solar Roller" : 186, "H2GO" : 187, "The DevasTater" : 188, "Creepy Crawler" : 189, "The Goblin" : 190, "Something Wicked" : 191, "Frosted Roller" : 192, "Gingerbread GT" : 193, "Holiday Heat" : 194, "Cold Snap" : 195, "The Snowy Knight" : 196, "The Rocket Klaus" : 197, "Golden Ticket" : 198, "Wavebreaker" : 199, "Broadwing" : 200, "Bimmer Prism i20" : 201, "Heartbreaker" : 202, "The Danger 9" : 203, "The Wild 500" : 204, "Tigreen" : 205, "X1 Eclipse" : 206, "Error 500" : 207, "Vapor" : 208, "9 Bit Racer" : 209}
+carsdict = {
+    "Lamborgotti Mephisto SS": 1,
+    "Lamborgotti Mephisto": 2,
+    "Jeepers Rubricorn": 3,
+    "Portch Picante": 4,
+    "Bantly Super Sport": 5,
+    "The Rolls": 6,
+    "Winston Citroen": 7,
+    "Winston Agile": 8,
+    "Rental Car": 9,
+    "Mission Accomplished": 10,
+    "Buggani Vyrus SS": 11,
+    "Auttie B9": 13,
+    "Nitsua Lance 722": 14,
+    "Misoux Lion": 15,
+    "Misoux Toad": 16,
+    "Minnie the Cooper": 17,
+    "Nizza 350x": 18,
+    "One Ace": 19,
+    "Cougar Ace": 20,
+    "Rand Rover R/T": 21,
+    "B-Team Van": 22,
+    "Mercedex Bens V-20": 23,
+    "Mercedex Bens C-64": 24,
+    "Portch Spyder": 25,
+    "Auttie Roadster": 26,
+    "Bimmer M2.0": 27,
+    "Bimmer 9.0t": 28,
+    "Thunder Cougarbird": 29,
+    "Rat Rod Skully": 30,
+    "Outtie R11": 31,
+    "The Flamerod": 33,
+    "Valent Performo": 34,
+    "Portch GT3 RS": 35,
+    "Ponce de Leon": 36,
+    "'67 Shellback GT-500": 37,
+    "Road Warrior": 38,
+    "Linux Elise": 39,
+    "'69 Shellback RT-500": 40,
+    "The Gator": 42,
+    "Bastok Suprillia": 43,
+    "The Judge": 44,
+    "The Stallion": 45,
+    "The Macro": 46,
+    "The Fastback": 47,
+    "The Covenant": 48,
+    "The Trifecta": 49,
+    "8 Bit Racer": 50,
+    "Mini Sherman": 51,
+    "Typiano Pizza Car": 52,
+    "Rocket Man": 53,
+    "All Terrain Vehicle": 54,
+    "MP 427": 55,
+    "The Wambulance": 56,
+    "The Hotdog Mobile": 57,
+    "F-35 JSF": 58,
+    "NASA Shuttle": 59,
+    "Caterham Racer": 60,
+    "Mack Daddy": 61,
+    "Big Hauler": 62,
+    "Big Blue": 63,
+    "Fort GT40": 64,
+    "Dom Vipper GST-R": 65,
+    "Alpha Romero 8Ω": 66,
+    "Blazing Buggy": 67,
+    "F4U Corsair": 68,
+    "Rocket Sleigh": 69,
+    "XMaxx Tree Racer": 70,
+    "Shadow XMaxx Tree": 71,
+    "Party Sleigh": 72,
+    "Zonday Tricolore": 73,
+    "The Monster": 74,
+    "Flux Capacitor": 75,
+    "The Gotham": 76,
+    "The Pirc": 77,
+    "Suziki GXRS 1200": 78,
+    "EZ Rider": 79,
+    "Lamborgotti AdventX": 80,
+    "Summer Classic": 81,
+    "Hang Ten": 82,
+    "'41 Woodie Deluxx": 83,
+    "Hang Eleven": 84,
+    "'41 Woodie Sunshine": 85,
+    "The Xcelsior V12": 86,
+    "'68 Roadtripper": 87,
+    "Hang Fifteen": 88,
+    "Wach 6": 89,
+    "Fort F-125": 90,
+    "Wisker Electric": 91,
+    "'67 Vette": 92,
+    "MSG 01": 93,
+    "Fort Stallion": 94,
+    "Police Bimmer": 95,
+    "Auttie R-8.1": 96,
+    "Wampus": 97,
+    "Pumpkin Hauler": 98,
+    "Wreath Racer": 99,
+    "Santa's Buggy": 100,
+    "Travis' Car": 101,
+    "Dark Elf": 102,
+    "The Golden Gift": 103,
+    "Corndogs Car": 104,
+    "14 Mantaray": 105,
+    "Ferreti Samsher 458": 106,
+    "Lacan Hypersport": 107,
+    "Sun Buggie": 108,
+    "Hammer Wheels": 110,
+    "Kringle 4000": 111,
+    "Buddy's Snowmobile": 112,
+    "Kringle 4000 XL": 113,
+    "Buddy's Snowmorocket": 114,
+    "Six Four": 115,
+    "Six Four Plus Three": 116,
+    "The Midnight Hauler": 117,
+    "The Candy Hauler": 118,
+    "Kringle 5000": 119,
+    "Wrapped Wracer": 120,
+    "Wrapped Wracer GT": 121,
+    "Holiday Hero": 122,
+    "Kringle 5000 L.T.": 123,
+    "Mercedex McLaro SLR": 124,
+    "Floaty Blue": 125,
+    "B.O.A.T.": 126,
+    "I'm Spicy!": 127,
+    "Y.A.C.H.T.": 128,
+    "Mercedex McLaro SLR 12.5": 129,
+    "Nitr-o'-Lantern": 130,
+    "Nitr-o'-the-Wisp": 131,
+    "Xmaxx Xxpress": 132,
+    "XMaxx Xxpress XXL": 133,
+    "Gilded Xxpress": 134,
+    "Lamborgotti Xmaxx LT": 135,
+    "Lamborgotti Xmaxx LT-C": 136,
+    "Mercedex McLaro SHS 15.0": 137,
+    "Strykist 1300": 138,
+    "Range Runner": 139,
+    "Strykist 1300 XT-LR": 140,
+    "Track-o-Lantern": 141,
+    "Gingerbread Racer": 142,
+    "Gingerbread Racer H&T": 143,
+    "Missile Toe": 144,
+    "Missile Toe H&T": 145,
+    "The Dark Chocolate Knight": 146,
+    "Tegglsa": 149,
+    "Egg Beater": 150,
+    "Eggcedes": 151,
+    "Egg Hauler": 152,
+    "Mercedex GT 20.0": 153,
+    "Rocky Roo": 154,
+    "NitroPAC": 155,
+    "Matchbox": 156,
+    "Lucky Number 7": 157,
+    "Easy Breezy": 158,
+    "HoverJet 5000 Mk. 3": 159,
+    "Golden Breeze": 160,
+    "B.U.S.": 161,
+    "S'cool B.U.S.": 162,
+    "AU-79": 163,
+    "The Underachiever": 164,
+    "The Overachiever": 165,
+    "The Wildflower": 166,
+    "Jolly RS": 167,
+    "Jolly GTX LG": 168,
+    "The Goldray": 169,
+    "can hav nt g0ld plx?": 170,
+    "The Wraptor": 171,
+    "Travis' Truck": 172,
+    "The Wraptor GG": 173,
+    "The Silent Knight": 174,
+    "NT Gold": 175,
+    "Lamborgotti Tiesto": 176,
+    "Portch Cobalt": 177,
+    "Alpha Romero 123Ω": 178,
+    "Travis' Big Truck": 179,
+    "Bright Idea": 180,
+    "The Sandstorm": 181,
+    "The Jury": 182,
+    "The Goldfish": 183,
+    "Shock Value": 184,
+    "Gold Standard": 185,
+    "Solar Roller": 186,
+    "H2GO": 187,
+    "The DevasTater": 188,
+    "Creepy Crawler": 189,
+    "The Goblin": 190,
+    "Something Wicked": 191,
+    "Frosted Roller": 192,
+    "Gingerbread GT": 193,
+    "Holiday Heat": 194,
+    "Cold Snap": 195,
+    "The Snowy Knight": 196,
+    "The Rocket Klaus": 197,
+    "Golden Ticket": 198,
+    "Wavebreaker": 199,
+    "Broadwing": 200,
+    "Bimmer Prism i20": 201,
+    "Heartbreaker": 202,
+    "The Danger 9": 203,
+    "The Wild 500": 204,
+    "Tigreen": 205,
+    "X1 Eclipse": 206,
+    "Error 500": 207,
+    "Vapor": 208,
+    "9 Bit Racer": 209
+}
+
+
 async def fetchText(session, url):
     async with session.get(url) as response:
         return await response.text()
+
 
 async def fetchRaw(session, url):
     async with session.get(url) as response:
@@ -21,7 +227,9 @@ async def userID(session, username):
     html_0 = req.split('RACER_INFO: ')
     html_1 = html_0[1].split(",")
     html_2 = html_1[0].split(":")
-    return(str(html_2[1]))
+    return (str(html_2[1]))
+
+
 async def get_profile(session, username: str):
     """
     Gets a racer's profile and team data
@@ -30,11 +238,13 @@ async def get_profile(session, username: str):
     playerUserId = await userID(session, username)
     playerLink = "https://www.nitrotype.com/api/players/" + playerUserId
     #m = requests.get("https://www.nitrotype.com/api/players/" + playerUserId)
-    m = await fetchText(session,playerLink)
+    m = await fetchText(session, playerLink)
     #j = json.loads(m.text)
     j = json.loads(m)
     return j
-async def get_car(session,car_id: int, car_hue: int = 0, size: str = 'large'):
+
+
+async def get_car(session, car_id: int, car_hue: int = 0, size: str = 'large'):
     host = 'https://www.nitrotype.com/cars'
 
     if car_hue != 0:
@@ -49,39 +259,47 @@ async def get_car(session,car_id: int, car_hue: int = 0, size: str = 'large'):
     b.seek(0)
     return b
 
-async def getCarAndPaint(session,username):
+
+async def getCarAndPaint(session, username):
     playerUserId = await userID(session, username)
     playerLink = "https://www.nitrotype.com/api/players/" + playerUserId
-    m = await fetchText(session,playerLink)
+    m = await fetchText(session, playerLink)
     j = json.loads(m)
     carID = j["data"]["carID"]
     carPaint = j["data"]["carHueAngle"]
-    if(str(carPaint) == '0'):
-        carAndPaintLink = "https://www.nitrotype.com/cars/" + str(carID) + "_large_1.png"
+    if (str(carPaint) == '0'):
+        carAndPaintLink = "https://www.nitrotype.com/cars/" + str(
+            carID) + "_large_1.png"
     else:
-        carAndPaintLink = "https://www.nitrotype.com/cars/painted/" + str(carID) + "_large_1_" + str(carPaint) + ".png"
-    return(carAndPaintLink)
+        carAndPaintLink = "https://www.nitrotype.com/cars/painted/" + str(
+            carID) + "_large_1_" + str(carPaint) + ".png"
+    return (carAndPaintLink)
+
 
 async def compileProfile(username):
     async with aiohttp.ClientSession() as session:
         playerInfo = await get_profile(session, username)
-        return(playerInfo)
+        return (playerInfo)
+
+
 async def compileBytes(car_id: int, car_hue: int = 0, size: str = 'large'):
     async with aiohttp.ClientSession() as session:
-        a =  await get_car(session, car_id, car_hue, size)
-        return(a)
+        a = await get_car(session, car_id, car_hue, size)
+        return (a)
+
 
 async def compileLink(username):
     async with aiohttp.ClientSession() as session:
         a = await getCarAndPaint(session, username)
-        return(a)
+        return (a)
+
 
 async def compileProfileAsync(username):
     return await compileProfile(username)
 
+
 async def compileCarAsync(car_id: int, car_hue: int = 0, size: str = 'large'):
     return await compileBytes(car_id, car_hue, size)
-
 
 
 async def carLink(username):
@@ -92,7 +310,6 @@ def findCar(nameString):
     newDict = {}
     carInfo = []
     for x in carsdict:
-        if(nameString.lower() in x.lower()):
+        if (nameString.lower() in x.lower()):
             newDict[x] = str(carsdict[x])
-    return(newDict)
-
+    return (newDict)
